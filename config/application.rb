f026bba7a1c186551b18945ca4b744a7f50ad97c
sakarin <sakarin.my@gmail.com>
2013-04-02 11:27:08 +0700

    edit asset pipeline false

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module TicketSystem
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
