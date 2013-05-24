11204fd7b82c0d4e36142a8ccdec804fe2531b2b
sakarin <sakarin.my@gmail.com>
2013-05-24 08:06:03 +0700

    done

--- config/application.rb
+++ config/application.rb
@@ -76,7 +76,7 @@ module TicketSystem
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    c# Setup for production - deliveries, no errors raised
+    # Setup for production - deliveries, no errors raised
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
